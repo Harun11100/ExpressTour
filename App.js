2d12fde54b8c5dad550164139aac5913a1c62824
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-08 21:34:33 +0600

    Learnt how respond the URL params to the server and get specific data through request

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ const app= express();
 app.use(express.json())// this is a middleware to send body data to request object
 
 
+// read the file fro the directory and stored in "tours" as a json format
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
+
+// this is to  read the data from the server using GET request
 
 app.get('/api/v1/tours',(req,res)=>{
 
@@ -19,8 +22,40 @@ app.get('/api/v1/tours',(req,res)=>{
   
       })
 })
- 
 
+
+// this is the process to define params to the https method and how to get specific data from the server through request 
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+      console.log(req.params)
+
+      const id=req.params.id*1
+      const tour=tours.find((el)=>
+            el.id===id
+
+      )
+      
+      if (id>tours.length){
+            res.status(404).json({
+                  status:'We coudnot find data'
+            })
+      }else{
+
+            res.status(200).json({
+                  status:'success',
+                  data:{
+                        tour
+                  }
+        
+            })
+      }
+      
+
+      
+})
+ 
+ 
+// This is the way to create new data to the server 
 
 app.post('/api/v1/tours',(req,res)=>{
 
@@ -45,6 +80,8 @@ app.post('/api/v1/tours',(req,res)=>{
      })
 })
 
+
+// this is to run the server at port 8000 on localhost 127.0.0.1
 const port =8000 
 app.listen(port,()=>{
       console.log(`App running on this port :${port}`)
