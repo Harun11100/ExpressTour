15fdbc6a1479cd2d4695b089921f347380c1d14e
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-08 22:47:57 +0600

    use routes to make the fuction reuseable and make it clean

--- App.js
+++ App.js
@@ -11,98 +11,113 @@ const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.
 
 // this is to  read the data from the server using GET request
 
-app.get('/api/v1/tours',(req,res)=>{
-
-      res.status(200).json({
-            status:'success',
-            result: tours.length,
-            data:{
-                  tours
-            }
-  
-      })
-})
 
 
-// this is the process to define params to the https method and how to get specific data from the server through request 
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-      console.log(req.params)
-
-      const id=req.params.id*1
-      const tour=tours.find((el)=>
-            el.id===id
-
-      )
-      
-      if (id>tours.length){
-            res.status(404).json({
-                  status:'We coudnot find data'
-            })
-      }else{
+export const getAllTours= () => {
+      (req,res)=>{
 
             res.status(200).json({
                   status:'success',
+                  result: tours.length,
                   data:{
-                        tour
+                        tours
                   }
         
             })
       }
       
-
+}
+// this is the process to define params to the https method and how to get specific data from the server through request 
+export const getTour=()=>{
+      (req,res)=>{
+            console.log(req.params)
       
-})
+            const id=req.params.id*1
+            const tour=tours.find((el)=>
+                  el.id===id
+      
+            )
+            
+            if (id>tours.length){
+                  res.status(404).json({
+                        status:'We coudnot find data'
+                  })
+            }else{
+      
+                  res.status(200).json({
+                        status:'success',
+                        data:{
+                              tour
+                        }
+              
+                  })
+            }
+            
+      
+            
+      }
+}
+
  
  
 // This is the way to create new data to the server 
 
-app.post('/api/v1/tours',(req,res)=>{
 
-     const newId=tours[tours.length-1].id+1
-     const newTour= Object.assign({
-      id:newId},
-      req.body
-     )
+export const createTour=()=>{
+      (req,res)=>{
 
-
-     tours.push (newTour)
-
-     fs.writeFile(`${dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
-
-            res.status(201).json({  //201 code for "Created" 200 for "OK"
-                  status:'success',
-
-                  data:{
-                        tour:newTour
-                  }
+            const newId=tours[tours.length-1].id+1
+            const newTour= Object.assign({
+             id:newId},
+             req.body
+            )
+       
+       
+            tours.push (newTour)
+       
+            fs.writeFile(`${dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+       
+                   res.status(201).json({  //201 code for "Created" 200 for "OK"
+                         status:'success',
+       
+                         data:{
+                               tour:newTour
+                         }
+                   })
             })
-     })
-})
-
+       }
+       
+       
+}
 
 // This is the process to update an specific value of an object .
-app.patch('/api/v1/tours/:id',(req,res)=>{
+
+export const updateTour=()=>{
+     (req,res)=>{
       
-      if(!req.params.id*1 > tours.length){
-            return res.status(404).json({
-                  status:'fail',
-                  message:'Invalid Id'
-            })
-      }
-      res.status(200).json({
-            status:'success',
-            data:{
-                  tour:'***Updated Tour***'
+            if(!req.params.id*1 > tours.length){
+                  return res.status(404).json({
+                        status:'fail',
+                        message:'Invalid Id'
+                  })
             }
-      })
-}) 
+            res.status(200).json({
+                  status:'success',
+                  data:{
+                        tour:'***Updated Tour***'
+                  }
+            })
+      } 
+      
+}
 
 
 // this is to delete an object from the server
 
-app.delete('/api/v1/tours/:id',(req,res)=>{
-        const id=req.params.id*1
+export const deleteTour=()=>{
+      (req,res)=>{
+      const id=req.params.id*1
        if(id>tours.length){
 
             const filteredItem=tours.filter((item)=>item.id===id)
@@ -114,9 +129,23 @@ app.delete('/api/v1/tours/:id',(req,res)=>{
             }
            })
       }
-      
-})
+      }
+}
+// app.get('/api/v1/tours',getAllTours())
+// app.get('/api/v1/tours/:id',getTour())
+// app.post('/api/v1/tours',createTour())
+// app.patch('/api/v1/tours/:id',updateTour())
+// app.delete('/api/v1/tours/:id',deleteTour())
 
+app
+.route('/api/v1/tours')
+.get(getAllTours)
+.post(createTour)
+
+app.route('/api/v1/tours/:id')
+.get(getTour)
+.patch(updateTour)
+.delete(deleteTour)
 
 
 // this is to run the server at port 8000 on localhost 127.0.0.1
