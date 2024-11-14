d39e0b0cc86af73ca55b06aff918365d15f350c2
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-14 22:28:10 +0600

    Learn to create middleare to share static files as well as leart to check environment variable

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-
+const dotenv=require('dotenv')
 const app=require('./App')
 
+// console.log(app.get('env'))
 
+dotenv.config({path:'./config.env'})
+console.log(process.env)
 const port =8000 
 app.listen(port,()=>{
       console.log(`App running on this port :${port}`)
